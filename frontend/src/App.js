dea9206a959e23cb30c311e278051dc39b22fd2e
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-06 20:39:34 -0500

    Frontend - Proyecto React Base
    se sube el proyecto base de react

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react'
+
+export const HOST_API = "http://localhost:8080/api";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
